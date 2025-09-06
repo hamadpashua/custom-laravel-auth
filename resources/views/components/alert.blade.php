6d2c5bc1f7643b13377b79163537d9c8e67f0cba
hamadpashua <102689093+hamadpashua@users.noreply.github.com>
2025-09-06 11:58:07 +0300

    adding sweetalert2 fron the alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,21 @@
 @if(session('success'))
-    <div class=" container alert alert-success">
+    {{-- <div class=" container alert alert-success">
         {{ session('success') }}
-    </div>
+    </div> --}}
+
+    @push('scripts')
+
+        <script>
+            Swal.fire({
+                timerProgressBar: true,
+                position: 'center',
+                icon: 'success',
+                title: "{{ session('success') }}",
+                showConfirmButton: true,
+                confirmButtonColor: "#3085d6",
+                timer: 2000
+            })
+        </script>
+    @endpush
+    
 @endif
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,16 +2,19 @@
 
 @section('content')
 
-    <div class="col-sm-8 py-5 mx-auto">
-        <h1 class="display-5 fw-normal">Welcome Back.</h1>
-        <p class="fs-5">This example shows how responsive offcanvas menus work within the navbar. For
-            positioning of navbars, checkout the <a href="/docs/5.3/examples/navbar-static/">top</a> and <a
-                href="/docs/5.3/examples/navbar-fixed/">fixed top</a> examples.</p>
+        <div class="col-sm-8 py-5 mx-auto">
+            <h1 class="display-5 fw-normal">Welcome Back.</h1>
+            <p class="fs-5">This example shows how responsive offcanvas menus work within the navbar. For
+                positioning of navbars, checkout the <a href="/docs/5.3/examples/navbar-static/">top</a> and <a
+                    href="/docs/5.3/examples/navbar-fixed/">fixed top</a> examples.</p>
 
-        <p> <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit" class="btn btn-primary">Log Out</button>
-        </form> </p>
-    </div>
+            <p>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit" class="btn btn-primary">Log Out</button>
+            </form>
+            </p>
+        </div>
 
 @endsection
+
